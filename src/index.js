80390f4d20a021f6e0aff8684a150d0b7aba1818
Jacopo <jacomig@gmail.com>
2020-04-10 15:17:59 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Motion, spring} from 'react-motion';
 
 class App extends React.Component {
     render(){
         return (
             <div>
-                <Motion defaultStyle={{x: 0}} style={{x: spring(10)}}>
-                {value => <div>{Math.floor(value.x)}</div>}
-                </Motion>
+               APPPPPPPP
             </div>
         )
     }
